9c9a25137d6455ef5c58acf8c1a6869174a3cd21
sonykuriakoseantony <sonykuriakoseantony@gmail.com>
2026-01-17 15:05:48 +0530

    Notes manager using nextjs is completed

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,20 +1,62 @@
 import notes from "@/app/models/notes";
 import { connectDB } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  try {
-    await connectDB();
-    const { id } = params;
+// steps to resolve get a single notes api - GET
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        await connectDB();
+        const { id } = await params;
 
-    const note = await notes.findById(id);
-    if (!note) {
-      return NextResponse.json({ message: "Note not found" }, { status: 404 });
+        const note = await notes.findById(id);
+
+        if (!note) {
+            return NextResponse.json({ message: "Note not found" }, { status: 404 });
+        }
+        else {
+            return NextResponse.json(note, { status: 200 });
+        }
+
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
     }
+}
 
-    return NextResponse.json(note, { status: 200 });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
-  }
+// steps to resolve update note api - PUT
+export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        await connectDB();
+        const reqBody = await req.json();
+        const { id } = await params;
+
+        const note = await notes.findByIdAndUpdate(id,reqBody,{new : true});
+
+        if (!note) {
+            return NextResponse.json({ message: "Note not found" }, { status: 404 });
+        }
+        else {
+            return NextResponse.json(note, { status: 200 });
+        }
+
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    }
+}
+
+// steps to resolve delete note api - DELETE
+export async function DELETE(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
+        await connectDB();
+        const { id } = await params;
+
+        const note = await notes.findByIdAndDelete(id);
+
+        return NextResponse.json(note, { status: 200 });
+
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    }
 }
--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,26 +1,28 @@
 import notes from "@/app/models/notes";
 import { connectDB } from "@/lib/mongodb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // steps to resolve add note api - POST
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
     try {
         await connectDB();
         const reqBody = await req.json();
         const newNote = await notes.create(reqBody);
 
-        console.log(newNote);
-
+        //send response to client
         return NextResponse.json(newNote, { status: 201 })
+        
     } catch (err) {
         console.log(err);
         return NextResponse.json(err, { status: 500 })
     }
 }
 
+// steps to resolve get all notes api - GET
 export async function GET() {
     try{
 
+        await connectDB();
         const allNotes = await notes.find();
         console.log(allNotes);
 
